f310434586adc3190fb80b8324889776cefc017b
Chuck Walbourn <chuckw@windows.microsoft.com>
2020-12-29 14:46:16 -0800

    Fix command-line tools

--- Meshconvert/Meshconvert.cpp
+++ Meshconvert/Meshconvert.cpp
@@ -20,10 +20,10 @@
 #define NOHELP
 #pragma warning(pop)
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
-
+#include <cassert>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
 #include <fstream>
 #include <memory>
 #include <list>
